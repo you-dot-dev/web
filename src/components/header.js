4ea950ff8c9fb189e6cb8c9cf54afa00115c568c
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-01-28 16:02:27 -0800

    major commit, photos/svg, layout, navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,25 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import userPNG from '../images/user.svg'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <nav>
+      <ul className="header-links">
+        <li><Link to="/">{siteTitle}</Link></li>
+        <li><Link to="/articles">Articles</Link></li>
+        <li><Link to="/screencasts">Screen Casts</Link></li>
+        <li><Link to="/skillmap">Skill Map</Link></li>
+      </ul>
+      <ul className="account-menu">
+        <li>
+          <Link to="/account">
+            <img src={userPNG}  style={{height:'40px', width:"40px"}} alt="user icon"/>
+          </Link>
+        </li>
+      </ul>  
+    </nav>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import React from "react"
 
-import youDevLogo from "../images/you-dev-logo.png"
-import "../components/layout.css";
+import "../components/layout.scss";
 import SEO from "../components/seo"
+import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <div className="coming-soon">
+  <Layout>
     <SEO title="Home" />
-    <img src={youDevLogo} alt="You.Dev logo" />
+    <div className="parallax"></div>
+    <div className="coming-soon">
     <h2>coming soon!</h2>
     <p>Fun and fast full-stack training for engineers of all levels.</p>
-    <p>Angelika was here.</p>
-    <a href="mailto:paul@you.dev">Contact Us</a> for jobs opportunities.
-  </div>
+    </div>
+  </Layout>
 )
 
 export default IndexPage
