c7fca7a9ba507554cb66107ca3a9bff7173036ca
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-02-02 10:33:30 -0800

    Add initial register page

---
+++ src/pages/register.js
@@ -0,0 +1,82 @@
+import React, {useState, useContext} from 'react'
+import axios from 'axios'
+import Layout from '../components/layout'
+import AuthContext from '../contexts/auth/AuthContext'
+
+const Register = () => {
+
+  const authContext = useContext(AuthContext);
+
+  const { registerUser } = authContext;
+
+  console.log("authContext?:", authContext);
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUserName] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    registerUser({username, email, password});
+    /*
+    axios.post('http://localhost:7890/auth/register', {
+      username,
+      email,
+      password
+     })
+     .then(res => {
+       console.log(res.data)
+      //  props.setCurrentUser(res.data[0]);
+      //  props.setLoggedIn(true);
+     })
+     .catch(err => console.log(err))
+     */
+   }
+
+  return (
+    <Layout>
+<div className="container">
+
+     <div className="form-wrap">
+     
+      <h1>welcome to you.dev</h1>
+          <p>it's free and only takes a minute</p>
+  
+      <div className="form-group">
+          <label htmlFor="text">username</label>
+        <input 
+            type="text"
+            name="userName"
+            onChange={(e) => setUserName(e.target.value)}
+          /></div>
+      <div className="form-group">
+          <label htmlFor="text"> email address</label>
+        <input 
+          type="text"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        /></div>
+      <div className="form-group">
+          <label htmlFor="text">enter password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        /></div>
+      
+
+      <button
+        className="reg-log"
+        type="submit"
+        onClick={(e) => {handleSubmit(e)}}>sign up </button>
+
+       <p className="bottom-text"> By clicking the Sign Up button, you agree to our Terms and Conditions and Privacy Policy</p>
+    </div>
+      
+    </div>
+    </Layout>
+  )
+}
+
+
+
+export default Register
