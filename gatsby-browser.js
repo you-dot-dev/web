20730e0ff03a666936db3d6aee365a67962bc55a
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-02-02 10:27:28 -0800

    Add AuthContext provider to App root

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react';
+import AuthState from './src/contexts/auth/AuthState';
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <AuthState>{element}</AuthState>
+  );
+}
