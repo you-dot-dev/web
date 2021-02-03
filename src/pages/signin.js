4c165696209f725d33afd11966241b4d4e6e3465
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-02-03 14:09:29 -0800

    signin page boiler plate no styling yet

---
+++ src/pages/signin.js
@@ -0,0 +1,73 @@
+import React, {useState, useContext} from 'react'
+import axios from 'axios'
+import Layout from '../components/layout'
+import AuthContext from '../contexts/auth/AuthContext'
+
+const SignIn = () => {
+
+  const authContext = useContext(AuthContext);
+
+  const { signInUser } = authContext;
+;
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    signInUser({ email, password});
+    /*
+    axios.post('http://localhost:7890/auth/register', {
+      username,
+      email,
+      password
+     })
+     .then(res => {
+       console.log(res.data)
+      //  props.setCurrentUser(res.data[0]);
+      //  props.setLoggedIn(true);
+     })
+     .catch(err => console.log(err))
+     */
+   }
+
+  return (
+    <Layout>
+<div className="container">
+
+     <div className="form-wrap">
+     
+      <h1>login page</h1>
+          <p>welcome back, ready to make some progress?</p>
+  
+      <div className="form-group">
+          <label htmlFor="text"> email address</label>
+        <input 
+          type="text"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        /></div>
+      <div className="form-group">
+          <label htmlFor="text">password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        /></div>
+      
+
+      <button
+        className="btn"
+        type="submit"
+        onClick={(e) => {handleSubmit(e)}}>sign in</button>
+
+    </div>
+      
+    
+    </div>
+    </Layout>
+  )
+}
+
+
+
+export default SignIn
