76e35e3b2c91b0130b7c9bfdda1b4c93a9f91170
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-02-03 14:01:19 -0800

    boiler plate leaflet js skillmap loads on the page

--- src/pages/skillmap.js
+++ src/pages/skillmap.js
@@ -7,7 +7,7 @@ const skillMap = () => {
   return (
     <Layout>
       <MapContainer id="skill-map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
+      <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
