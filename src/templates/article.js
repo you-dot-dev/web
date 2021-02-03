3213b8186b3227c11f88b247aa3ca89290558e7c
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-02-02 16:26:58 -0800

    Update display date on each article

--- src/templates/article.js
+++ src/templates/article.js
@@ -7,12 +7,14 @@ const Template = ({data}) => {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
 
+  const displayDate = new Date(frontmatter.date).toDateString();
+
   return (
     <Layout>
       <div className="blog-post-container">
         <div className="blog-post">
           <h1>{frontmatter.title}</h1>
-          <h2>{frontmatter.date}</h2>
+          <h2>{displayDate}</h2>
           <h3>{frontmatter.author}</h3>
           <div
             className="blog-post-content"
