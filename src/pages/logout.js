2e66c57687ca59b12edf964dc059eac99cd75b8d
Paul Coe <pcoe@tesla.com>
2021-02-09 23:28:26 -0800

    Add barebones logout page

---
+++ src/pages/logout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Logout = () => {
+  return (
+    <Layout>
+      You have been logged out
+    </Layout>
+  );
+}
+
+
+export default Logout
