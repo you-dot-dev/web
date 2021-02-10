2eaeb980526e1010f966a75c586abb307bdd09a5
Paul Coe <pcoe@tesla.com>
2021-02-09 17:51:09 -0800

    Add test map and new map style

--- src/pages/skillmap.js
+++ src/pages/skillmap.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import '../components/skill-map.scss';
 import Layout from '../components/layout';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import FreeMap from '../images/test-map.jpg';
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet'
 
 const skillMap = () => {
   return (
     <Layout>
-      <MapContainer id="skill-map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
+      <MapContainer id="skill-map" center={[0, 0]} zoom={15} scrollWheelZoom={false}>
+        <ImageOverlay
+          url={FreeMap}
+          bounds={[
+            [-0.1,-0.1],
+            [0.1, 0.1]
+          ]}/>
+        <Marker position={[0.0, 0.0]}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            Getting Started
+          </Popup>
+        </Marker>
+        <Marker position={[0.0, 0.01]}>
+          <Popup>
+            <iframe width={"560"} height={"315"} src={"https://www.youtube.com/embed/Pr_-AudwsNU"} frameborder={"0"} allow={"autoplay; clipboard-write; encrypted-media; picture-in-picture"} allowfullscreen={true}></iframe>
           </Popup>
         </Marker>
       </MapContainer>
