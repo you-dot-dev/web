96db9546a20e2088bb472f9c4c05add258853bb3
Paul Coe <pcoe@tesla.com>
2021-02-09 23:20:17 -0800

    Remove Link dependency, add === comparison

--- src/pages/screencasts.js
+++ src/pages/screencasts.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -14,7 +14,7 @@ const Screencasts = ({data}) => {
   .map( (article) => {
 
     let [articleImage] = data.allImageSharp.nodes.filter( (image) => {
-      return article.frontmatter.image == image.fluid.originalName
+      return article.frontmatter.image === image.fluid.originalName
     });
 
     const displayDate = new Date(article.frontmatter.date).toDateString();
