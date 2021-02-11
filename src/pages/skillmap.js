ea4c8dd939f22d36075b0d1c3005dbcdd2ecc992
Paul Coe <pcoe@tesla.com>
2021-02-11 14:23:25 -0800

    Testing ImageOverlay with svg

--- src/pages/skillmap.js
+++ src/pages/skillmap.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import '../components/skill-map.scss';
 import Layout from '../components/layout';
-import FreeMap from '../images/map.svg';
+import Freeland from "../images/freeland.svg";
+import TheSea from "../images/the-sea.svg";
+import Words from "../images/words.svg";
 import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet'
 
 const skillMap = () => {
+  const defaultBounds = [[-1,-1], [1,1]];
+
   return (
     <Layout>
-      <MapContainer id="skill-map"
-        center={[0, 0]}
-        zoom={10} scrollWheelZoom={false} maxBounds={[[-1,-1], [1,1]]} >
-        <ImageOverlay
-          url={FreeMap}
-          bounds={[
-            [-1,-1],
-            [1, 1]
-          ]}/>
+      <MapContainer id={"skill-map"}
+        center={[0,0]}
+        minZoom={10} zoom={13} maxZoom={14}
+        scrollWheelZoom={true}
+        maxBounds={defaultBounds}
+        maxBoundsViscosity={1.0}
+      >
+
+        <ImageOverlay url={Words} zIndex={2} bounds={defaultBounds} />
+        <ImageOverlay url={Freeland} zIndex={1} bounds={defaultBounds} />
+        <ImageOverlay url={TheSea}   zIndex={0} bounds={defaultBounds} />
         <Marker position={[0.0, 0.0]}>
           <Popup>
             Getting Started
           </Popup>
         </Marker>
-        <Marker position={[0.0, 0.01]}>
+        <Marker position={[0.0, 0.01]} className={"completed"}>
           <Popup>
             <iframe title="HTML" width={"560"} height={"315"} src={"https://www.youtube.com/embed/Pr_-AudwsNU"} frameborder={"0"} allow={"autoplay; clipboard-write; encrypted-media; picture-in-picture"} allowfullscreen={true}></iframe>
           </Popup>
