893a15af91beec7fb046bd30626bf895386ef297
Paul Coe <pcoe@tesla.com>
2021-02-13 15:13:28 -0800

    Merge transition and cloud layer commits

--- src/pages/skillmap.js
+++ src/pages/skillmap.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../components/skill-map.scss';
 import Layout from '../components/layout';
+import Zones from "../images/zones-01.svg";
 import Freeland from "../images/freeland.svg";
 import TheSea from "../images/the-sea.svg";
 import Words from "../images/words.svg";
-import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
+import CloudLayer from "../images/cloud-layer-01.svg";
 import PageTransition from 'gatsby-plugin-page-transitions';
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
+
 
 const skillMap = () => {
   const defaultBounds = [[-1,-1], [1,1]];
@@ -15,15 +18,17 @@ const skillMap = () => {
       <PageTransition>
       <MapContainer id={"skill-map"}
         center={[0,0]}
-        minZoom={10} zoom={13} maxZoom={14}
+        minZoom={11} zoom={13} maxZoom={14} onZoomend={(event) => {console.log("event:", event)}}
         scrollWheelZoom={true}
-        maxBounds={defaultBounds}
+        maxBounds={[[-0.5, -0.5],[0.75, 0.5]]}
         maxBoundsViscosity={1.0}
       >
 
-        <ImageOverlay url={Words} zIndex={2} bounds={defaultBounds} />
-        <ImageOverlay url={Freeland} zIndex={1} bounds={defaultBounds} />
-        <ImageOverlay url={TheSea}   zIndex={0} bounds={defaultBounds} />
+        <ImageOverlay url={CloudLayer} zIndex={4} bounds={defaultBounds} />
+        <ImageOverlay url={Zones}      zIndex={3} bounds={defaultBounds} />
+        <ImageOverlay url={Words}      zIndex={2} bounds={defaultBounds} />
+        <ImageOverlay url={Freeland}   zIndex={1} bounds={defaultBounds} />
+        <ImageOverlay url={TheSea}     zIndex={0} bounds={defaultBounds} />
         <Marker position={[0.0, 0.0]}>
           <Popup>
             Getting Started
