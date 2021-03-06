580aadd55edeb42f16b3c6a1754e72f9b76da2a5
Paul Coe <pcoe@tesla.com>
2021-03-06 15:05:48 -0800

    Add plugin-react-leaflet, create-client-paths to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,48 +10,45 @@ module.exports = {
 
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-page-transitions`,
-    {
-      resolve: `gatsby-plugin-scroll-reveal`,
+
+    { resolve: `gatsby-plugin-scroll-reveal`,
       options: {
-          threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
-          once: true, // Defines if animation needs to be launched once
-          disable: false, // Flag for disabling animations
-          
-          // Advanced Options
-          selector: '[data-sal]', // Selector of the elements to be animated
-          animateClassName: 'sal-animate', // Class name which triggers animation
-          disabledClassName: 'sal-disabled', // Class name which defines the disabled state
-          rootMargin: '0% 50%', // Corresponds to root's bounding box margin
-          enterEventName: 'sal:in', // Enter event name
-          exitEventName: 'sal:out', // Exit event name
+        threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
+        once: true, // Defines if animation needs to be launched once
+        disable: false, // Flag for disabling animations
+
+        // Advanced Options
+        selector: '[data-sal]', // Selector of the elements to be animated
+        animateClassName: 'sal-animate', // Class name which triggers animation
+        disabledClassName: 'sal-disabled', // Class name which defines the disabled state
+        rootMargin: '0% 50%', // Corresponds to root's bounding box margin
+        enterEventName: 'sal:in', // Enter event name
+        exitEventName: 'sal:out', // Exit event name
       }
     },
 
-    {
-      resolve: `gatsby-source-filesystem`,
+    { resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
 
-    {
-      resolve: `gatsby-source-filesystem`,
+    { resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
         path: `${__dirname}/src/articles`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
+
+    { resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
         path: `${__dirname}/src/screencasts`,
       },
     },
 
-    {
-      resolve: `gatsby-transformer-remark`,
+    { resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
@@ -68,8 +65,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
 
-    {
-      resolve: `gatsby-plugin-manifest`,
+    { resolve: `gatsby-plugin-manifest`,
       options: {
         name: `You . Dev`,
         short_name: `you.dev`,
@@ -81,12 +77,17 @@ module.exports = {
       },
     },
 
-    {
-      resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/pro/articles/*`, `/pro/screencasts/*`] },
+    { resolve: 'gatsby-plugin-react-leaflet',
+      options: {
+        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
+      }
+    },
+
+    { resolve: `gatsby-plugin-create-client-paths`,
+      options: {
+        prefixes: [`/pro/articles/*`, `/pro/screencasts/*`]
+      },
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+
+  ]
 }
