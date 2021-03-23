410d10093d6b6a9c1944bf84d0f44ec054ff8cbd
Paul Coe <pcoe@tesla.com>
2021-03-23 12:36:38 -0700

    Add Stripe Elements provider to wrapRootElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,9 +8,15 @@
 
 import React from 'react';
 import AuthState from './src/contexts/auth/AuthState';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_KEY);
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <AuthState>{element}</AuthState>
+    <Elements stripe={stripePromise}>
+      <AuthState>{element}</AuthState>
+    </Elements>
   );
 }
