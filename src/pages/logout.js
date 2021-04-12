42fd350d007817715b968903ed85bee0e0639143
Paul Coe <pcoe@tesla.com>
2021-04-12 12:52:17 -0700

    Add AuthContext to logout page and invoke logoutUser() function

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Layout from "../components/layout";
+import AuthContext from "../contexts/auth/AuthContext";
+import axios from "axios";
 
 const Logout = () => {
+  const authContext = useContext(AuthContext);
+
+  const { logoutUser } = authContext;
+
+  useEffect( () => {
+
+    console.log("logging out user...");
+    logoutUser();
+    console.log("user logged out!");
+
+    return () => {
+    }
+
+  }, []);
+
   return (
     <Layout>
       You have been logged out
