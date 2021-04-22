b406a7bc12db916df1dedef902c0f113c171a139
Paul Coe <pcoe@tesla.com>
2021-04-22 10:02:09 -0700

    Use YOUDEV_API_URL instead of localhost

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -5,33 +5,31 @@ import { navigate, Link} from 'gatsby';
 import Spinner from '../components/spinner';
 import PageTransition from 'gatsby-plugin-page-transitions';
 import SEO from "../components/seo";
+import axios from "axios";
 
 const SignIn = (props) => {
 
   const authContext = useContext(AuthContext);
 
   const { signInUser, loading } = authContext;
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  const [ password, setPassword] = useState('');
+  const [ email, setEmail] = useState('');
 
   function handleSubmit(e){
     e.preventDefault();
     signInUser({ email, password});
     navigate('/skillmap')
-    /*
-    axios.post('http://localhost:7890/auth/register', {
-      username,
-      email,
-      password
-     })
-     .then(res => {
-       console.log(res.data)
-      //  props.setCurrentUser(res.data[0]);
-      //  props.setLoggedIn(true);
-     })
-     .catch(err => console.log(err))
-     */
-   }
+  }
+
+  function signinGoogle(e) {
+    axios.get(`${YOUDEV_API_URL}/auth/google`, { withCredentials: true })
+      .then( ({data}) => {
+        navigate(data.authorizationUrl);
+      })
+      .catch( (err) => {
+        console.log("signinGoogle err:", err)
+      });
+  }
 
 
   return (
@@ -75,14 +73,14 @@ const SignIn = (props) => {
 
       <div className="outside-signin">
         <div className="button">
-        <button>sign in with google <i class="fab fa-google"></i></button><br/>
+        <button onClick={ (e) => {signinGoogle(e)}} >sign in with google <i class="fab fa-google"></i></button><br/>
        
         </div>
         <div className="button">
         <button>sign in with github <i class="fab fa-github"></i></button>
         </div>
       </div>
-        <p className="signin-container register-container">don't have an account? <Link to='/register'>register here</Link></p>
+        <p className="signin-container register-container">don't have an account? <Link to="/register">register here</Link></p>
     
 
     </PageTransition>
