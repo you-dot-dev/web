d0d852a36759dbd9cd87b66d9da3e3d04d8dfd41
Paul Coe <pcoe@tesla.com>
2021-05-12 15:45:29 -0700

    Add Stripe Provider for application

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,14 +9,27 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+
 import Header from "./header"
 import Footer from './footer';
 import "./layout.scss"
 import "./horizontal-rule.scss"
 import './signinRegister.scss'
 import './article.scss'
+import './account-info.scss'
+import './manage.scss'
 import './account.scss'
 import './footer.scss'
+
+const {
+  STRIPE_KEY
+} = process.env;
+
+const stripePromise = loadStripe(STRIPE_KEY);
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -30,11 +43,11 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <Elements stripe={stripePromise}>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <main>{children}</main>
       <Footer/>
-    </>
+    </Elements>
   )
 }
 
